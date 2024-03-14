d603fdd5e8b789ab9f8062740fa8a6449e2bd22d
Nick da silva <nickdasilva@ip-192-168-5-142.eu-west-2.compute.internal>
2024-03-14 22:36:55 +0000

    Add underline to active header nav. Add corresponding tests.

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,16 +1,20 @@
 import { Title, Text } from '@mantine/core';
+
 import classes from './Welcome.module.css';
 
-export const Welcome: React.FunctionComponent = () => (
+export const Welcome: React.FunctionComponent<{
+  isNewRecord: boolean;
+  isExistingRecords: boolean;
+}> = ({ isNewRecord, isExistingRecords }) => (
   <div>
     <Title className={classes.title} ta="center" mt={50} mb="lg">
       Cy&apos;s sleep tracking app!
     </Title>
     <div className={classes.links}>
-      <Text component="a" href="/">
+      <Text component="a" href="/" td={isNewRecord ? 'underline' : ''}>
         Submit a new record
       </Text>
-      <Text ml="lg" component="a" href="/logs">
+      <Text ml="lg" component="a" href="/logs" td={isExistingRecords ? 'underline' : ''}>
         Review existing records
       </Text>
     </div>
--- pages/logs.tsx
+++ pages/logs.tsx
@@ -24,7 +24,7 @@ export default function HomePage({
   return (
     <>
       <Header>
-        <Welcome />
+        <Welcome isNewRecord={false} isExistingRecords />
       </Header>
       <main>
         <Container>
